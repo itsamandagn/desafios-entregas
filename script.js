7de537ece0fe3628081a7170f711ac65223500aa
Amanda <amandagn07@gmail.com>
2024-07-18 22:05:08 -0300

    entrega de desafio aula 2 algoritmo js simples

---
+++ script.js
@@ -0,0 +1,4 @@
+var nome = prompt ("insira aqui o seu nome");
+var singer = prompt("insira aqui o nome do seu cantor(a) ou banda favorita");
+var music = prompt("insira aqui a sua música favorita");
+alert("Olá! Meu nome é" + " " + nome + ", meu cantor favorito é" + " " + singer + " " + "e minha música favorita é" + " " + music);
